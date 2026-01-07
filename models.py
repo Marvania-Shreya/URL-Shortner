680625e3109637fdc219861492eff25d9b218a5c
Marvania-Shreya <smarvaina@gmail.com>
2026-01-07 13:38:48 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,50 +2,71 @@ import sqlite3
 
 DB_NAME = "database.db"
 
+def connect_db():
+    return sqlite3.connect(DB_NAME)
+
 def init_db():
-    with sqlite3.connect(DB_NAME) as conn:
-        conn.execute('''
-           CREATE TABLE IF NOT EXISTS urls(
-                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                     original_url TEXT NOT NULL,
-                     short_code TEXT UNIQUE NOT NULL,
-                     visit_count INTEGER DEFAULT 0
-                     )         
-    ''')
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS urls (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            original_url TEXT NOT NULL,
+            short_code TEXT UNIQUE NOT NULL,
+            clicks INTEGER DEFAULT 0
+        )
+    """)
+
+    conn.commit()
+    conn.close()
 
 def insert_url(original_url, short_code):
-    with sqlite3.connect(DB_NAME) as conn:
-        conn.execute('''
-            INSERT INTO urls (original_url, short_code)
-                     VALUES (?, ?)
-        ''', (original_url, short_code))
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO urls (original_url, short_code) VALUES (?, ?)",
+        (original_url, short_code)
+    )
+    conn.commit()
+    conn.close()
 
 def get_url(short_code):
-    with sqlite3.connect(DB_NAME) as conn:
-        cur = conn.execute('SELECT * FROM urls WHERE short_code = ?', (short_code, ))
-        return cur.fetchone()
-
-def increment_visit_count(short_code):
-    with sqlite3.connect(DB_NAME) as conn:
-        conn.execute('''
-            UPDATE urls
-            SET visit_count = visit_count + 1
-            WHERE short_code = ?                  
-        ''', (short_code,))
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM urls WHERE short_code = ?",
+        (short_code,)
+    )
+    data = cursor.fetchone()
+    conn.close()
+    return data
 
 def get_all_urls():
-    with sqlite3.connect(DB_NAME) as conn:
-        cur = conn.execute('SELECT original_url, short_code, visit_count FROM urls ORDER by id DESC')
-        return cur.fetchall()
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM urls")
+    data = cursor.fetchall()
+    conn.close()
+    return data
 
-def delete_url_by_code(short_code):
-    with sqlite3.connect(DB_NAME) as conn:
-        conn.execute('DELETE from urls WHERE short_code = ?', (short_code, ))
+def increase_click(short_code):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE urls SET clicks = clicks + 1 WHERE short_code = ?",
+        (short_code,)
+    )
+    conn.commit()
+    conn.close()
 
-def get_url_by_original(original_url):
-    with sqlite3.connect(DB_NAME) as conn:
-        cur = conn.execute(
-            'SELECT * FROM urls WHERE original_url = ?',
-            (original_url,)
-        )
-        return cur.fetchone()
+def delete_url(short_code):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "DELETE FROM urls WHERE short_code = ?",
+        (short_code,)
+    )
+    conn.commit()
+    conn.close()
+
